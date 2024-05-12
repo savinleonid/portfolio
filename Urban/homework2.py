0d8603319f3582c9aa78724131eb7e3f76da0aff
Leonid Savin <110335131+savinleonid@users.noreply.github.com>
2024-05-12 18:59:42 +0300

    Delete Urban/homework2.py

--- Urban/homework2.py
+++
@@ -1,8 +0,0 @@
-name = "Leonid"  # init name
-print("Name: " + name)  # print name
-age = 29  # init age
-print(f"Age: {age}")  # print age w/ f-string
-age += 1  # increase age by 1
-print("New Age:", age)  # print new age
-is_student = True  # init bool
-print("Is Student:", is_student)  # print bool
