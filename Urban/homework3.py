fdba732742eab6e24dcbd085618cf2b5dc073680
savinleonid <savinleonid94@gmail.com>
2024-05-12 19:53:20 +0300

    Практическая работа по уроку "Организация программ и методы строк."

---
+++ Urban/homework3.py
@@ -0,0 +1,8 @@
+my_string = input("Enter Random String: ")  # input string
+print("Length of String:", len(my_string))  # print length of the string
+
+print(my_string.upper())  # print string in upper-case
+print(my_string.lower())  # print string in lower-case
+print(my_string.replace(' ', ''))  # deleting whitespaces
+print("First letter:", my_string[0])  # first letter
+print("Last letter:", my_string[-1])  # last letter
