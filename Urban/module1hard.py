0e0f1faa56e78301a5d65988b35aa48b6e5975ee
savinleonid <savinleonid94@gmail.com>
2024-05-13 00:54:02 +0300

    Дополнительное практическое задание "Средний балл" по модулю: "Базовые структуры данных."

--- Urban/module1hard.py
+++ Urban/module1hard.py
@@ -7,6 +7,6 @@ students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 students = list(students)  # revert to list for accessibility
 students.sort()  # sort alphabetically
 grades = map(truediv, list(map(sum, grades)), list(map(len, grades)))  # find mean of sublists and reassign to grades
-dict_grades = dict(zip(students, grades))  # zip two list to dict
+dict_grades = dict(zip(students, grades))  # zip list and map dict
 
 print(dict_grades)
