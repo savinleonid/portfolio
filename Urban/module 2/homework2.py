054ccdcef77a3caf56dad053ad1d8dc6bc3d3ac0
savinleonid <savinleonid94@gmail.com>
2024-05-13 17:05:42 +0300

    Самостоятельная работа по уроку "Цикл for.

---
+++ Urban/module 2/homework2.py
@@ -0,0 +1,5 @@
+cars = ["BMW", "MB", "LADA", "KIA", "HONDA"]  # init list of cars
+cars_count = 0  # init counter
+for car in cars:
+    print('Я езжу на автомабиле марки', car)  # print on each line
+    cars_count += 10  # counter + 10
