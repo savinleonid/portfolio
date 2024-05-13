1235911a81766ea24040e1e441daa2214d61f556
Leonid Savin <110335131+savinleonid@users.noreply.github.com>
2024-05-13 21:57:55 +0300

    Дополнительное практическое задание по модулю: "Базовые структуры данных."

---
+++ Urban/module 2/module2hard.py
@@ -0,0 +1,29 @@
+def decipher(num: int):
+    """
+    Decipher.
+    Finds corresponding password solution for final additional homework module 2.
+    Takes integer from 3 to 20 and returns concatenated password string of unique pairs that sum of each
+    is equal to given number.
+
+    :param num: int
+    :return str:
+    """
+    if 3 <= num < 20 and isinstance(num, int):
+        # start operations
+        password = []  # unique pairs will store here before concatenation
+        for i in range(1, num):  # first literal of pair. We don't want it to start with '0' so start from 1
+            for j in range(i, num):  # second literal of pair. Start iterating from first literal to get uniqueness
+                if i + j == num:  # check for summation condition
+                    password.append(str(i) + str(j))  # store concatenated pair in list
+
+        return str.join("", password)  # convert list values to string
+    else:
+        # if number is invalid
+        print("Invalid number. Must be an integer between 3 - 19(inclusive)")  # can go to log or raise error
+        return None
+
+
+# test
+for n in range(2, 20):
+    psw = decipher(n)
+    print(f"{n}-", psw)
