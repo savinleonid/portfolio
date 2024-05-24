bf47e3c9d8772f9a569fde0ef1ad4514c9f13584
savinleonid <savinleonid94@gmail.com>
2024-05-24 23:43:15 +0300

    Домашнее задание по уроку "Модули и пакеты"

---
+++ Urban/module4/homework 1/main.py
@@ -0,0 +1,8 @@
+"""
+Module testing
+"""
+from module1 import hello_world_1, hello_world_2  # partial import from other module
+
+# tests
+hello_world_1()
+hello_world_2()
---
+++ Urban/module4/homework 1/module1.py
@@ -0,0 +1,6 @@
+def hello_world_1():
+    print("Hello World 1")
+
+
+def hello_world_2():
+    print("Hello World 2")
