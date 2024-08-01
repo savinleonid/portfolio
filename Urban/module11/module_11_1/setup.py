1518b5ced33cb8939c60a1af278faac99a3a9d3f
savinleonid <savinleonid94@gmail.com>
2024-08-01 19:14:32 +0700

    Домашнее задание по теме "Обзор сторонних библиотек Python"

--- Urban/module11/module_11_1/setup.py
+++ Urban/module11/module_11_1/setup.py
@@ -2,11 +2,14 @@ import os
 import shutil
 import sys
 import subprocess
+import time
 
-ROOT_DIR = os.path.dirname(os.path.abspath(__package__))
+ROOT_DIR = sys.path[1]
 
 # implement pip as a subprocess:
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-r', 'requirements.txt'])
-os.chdir('.venv/Lib/site-packages/binance')
+while not os.path.exists(rf'{ROOT_DIR}/.venv/Lib/site-packages/binance'):
+    time.sleep(0.5)
 shutil.copy('patch/client.py', rf'{ROOT_DIR}/.venv/Lib/site-packages/binance/client.py')
 shutil.copy('patch/enums.py', rf'{ROOT_DIR}/.venv/Lib/site-packages/binance/enums.py')
+print("Done")
