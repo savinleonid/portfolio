1295bb11df36925154ad2145ecbeb1ab52d10834
savinleonid <savinleonid94@gmail.com>
2024-08-21 19:25:55 +0700

    Домашнее задание по теме "План написания админ панели"

---
+++ Urban/module14/module_14_4/crud_functions.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+connection = sqlite3.connect("not_telegram.db")
+cursor = connection.cursor()
+
+
+def initiate_db():
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS Products(
+            id INTEGER PRIMARY KEY,
+            title TEXT NOT NULL,
+            description TEXT,
+            price INTEGER NOT NULL
+        )
+        """
+    )
+    connection.commit()
+
+
+def get_all_products():
+    cursor.execute("SELECT * FROM Products")
+    products = cursor.fetchall()
+    connection.commit()
+    return products
+
---
+++ Urban/module14/module_14_4/module_14_4.py
@@ -0,0 +1,111 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+from crud_functions import *
+
+
+API = ""
+bot = Bot(token=API)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+initiate_db()
+products = get_all_products()
+
+kb = ReplyKeyboardMarkup(resize_keyboard=True)
+calc_button = KeyboardButton(text="Calculate")
+info_button = KeyboardButton(text="Info")
+buy_button = KeyboardButton(text="Buy")
+kb.insert(calc_button)
+kb.insert(info_button)
+kb.add(buy_button)
+
+inline_kb = InlineKeyboardMarkup()
+inline_buy_kb = InlineKeyboardMarkup()
+calc_norm_btn = InlineKeyboardButton(text="Calculate Norm", callback_data="calories")
+formula_btn = InlineKeyboardButton(text="Formula", callback_data="formulas")
+product1_btn = InlineKeyboardButton(text="Product 1", callback_data="product_buying")
+product2_btn = InlineKeyboardButton(text="Product 2", callback_data="product_buying")
+product3_btn = InlineKeyboardButton(text="Product 3", callback_data="product_buying")
+product4_btn = InlineKeyboardButton(text="Product 4", callback_data="product_buying")
+inline_kb.add(calc_norm_btn, formula_btn)
+inline_buy_kb.row(product1_btn, product2_btn, product3_btn, product4_btn)
+
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+
+@dp.message_handler(text="Calculate")
+async def main_menu(message: types.Message):
+    await message.answer("Choose option:", reply_markup=inline_kb)
+
+
+@dp.message_handler(text="Buy")
+async def get_buying_list(message: types.Message):
+    for product in get_all_products():
+        await message.answer(f"Name: {product[1]} | Description: {product[2]} | Price: {product[3]}")
+        with open(f"pics/prod{product[0]}.png", "rb") as img:
+            await message.answer_photo(img)
+    await message.answer("Choose product to buy:", reply_markup=inline_buy_kb)
+
+
+@dp.callback_query_handler(text="formulas")
+async def get_formulas(call: types.CallbackQuery):
+    await call.message.answer("10 x weight(kg) + 6.25 x height(cm) - 5 x age(y) + 5")
+    await call.answer()
+
+
+@dp.callback_query_handler(text="calories")
+async def set_age(call: types.CallbackQuery):
+    await call.message.answer("Enter your age.")
+    await UserState.age.set()
+    await call.answer()
+
+
+@dp.callback_query_handler(text="product_buying")
+async def send_confirm_message(call: types.CallbackQuery):
+    await call.message.answer("Purchase succeeded!")
+    await call.answer()
+
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message: types.Message, state):
+    await state.update_data(age=message.text)
+    await message.answer("Enter your height.")
+    await UserState.growth.set()
+
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message: types.Message, state):
+    await state.update_data(growth=message.text)
+    await message.answer("Enter your weight.")
+    await UserState.weight.set()
+
+
+@dp.message_handler(state=UserState.weight)
+async def set_weight(message: types.Message, state):
+    await state.update_data(weight=message.text)
+    data = await state.get_data()
+    norm_calories = 10 * float(data["weight"]) + 6.25 * float(data["growth"]) - 5 * int(data["age"]) + 5  # for men
+    await message.answer(f"Your calorie norm is {norm_calories:.2f}")
+    await state.finish()
+
+
+@dp.message_handler(commands=["start"])
+async def start(message: types.Message):
+    await message.answer("Hi! I am a bot helping with your health.", reply_markup=kb)
+
+
+@dp.message_handler()
+async def all_messages(message: types.Message):
+    await message.answer("Type command /start to start conversation.")
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
+    connection.close()
